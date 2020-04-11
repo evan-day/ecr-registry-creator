f5e1bbce6245767564b694521a33e9fef6bcfbdf
evan-day <evan@evanday.dev>
2020-04-11 15:19:09 +0100

    added script and lifecycle policy

---
+++ registryCreator.sh
@@ -0,0 +1,6 @@
+echo "** AWS ECR Registry Creator **"
+read -p "Enter the name you want for your registry: " registryName
+echo "** Creating Registry **"
+aws ecr create-repository --repository-name $registryName
+echo "** Applying Standard Lifecycle Policy **"
+aws ecr put-lifecycle-policy --repository-name $registryName --lifecycle-policy-text "file://lifecyclePolicy.json"
